abc0783bc3daf3ea74f029ae2339a6f35cf40790
Akshay <akshaybhopani@gmail.com>
2020-10-04 12:58:35 +0530

    PIP Pro

---
+++ script.js
@@ -0,0 +1,32 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+const allow = document.getElementById('allow');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+button.addEventListener('click', async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
+
+// Even Listner
+
+allow.addEventListener('click', async () => {
+selectMediaStream();
+});
+
